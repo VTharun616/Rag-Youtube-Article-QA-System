d68ed4a8ade28d82605aaded0e85105263ab140d
Tharun Kumar <vaddetharun90@gmail.com>
2026-04-28 11:54:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 # ---------------- LLM ----------------
 llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-pro-latest",
+    model="gemini-1.5-flash",
     google_api_key=os.getenv("GOOGLE_API_KEY")
 )
 
