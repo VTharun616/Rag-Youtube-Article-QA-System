ec7d2add00b315c4dbee256695354127f48af4fb
Tharun Kumar <vaddetharun90@gmail.com>
2026-04-28 12:15:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ client = genai.Client(api_key="AIzaSyD15kFIDek_DjS55oKDYsyFusAGjQHDx00")  # 🔴
 
 def ask_llm(prompt):
     response = client.models.generate_content(
-        model="gemini-1.5-flash",
+        model="gemini-2.5-flash",
         contents=prompt
     )
     return response.text
